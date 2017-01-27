a830d4cd1190f1bb5f92ee7e91734c846d89cee4
Andaay <andybrbz@gmail.com>
2017-01-26 16:43:19 -0800

    final stream code ready for review

--- app.py
+++ app.py
@@ -1,58 +1,81 @@
-#! twitter/bin/python3
-
 import tweepy
 import time, sys, json
+import os, errno
 
 # this information should probably be encrypted
-access_token = "240097377-zXq4uXzgKnrSYy80JC5OU6hAhhOv9375kYFaQn6G"
-access_token_secret = "ik4cVFmbLZcJc78tK8W22oQrciPF7RSxgOMkAZRd1yFXG"
-consumer_key = "mqmJvvCHbgr39xmVsIeaJScrG"
-consumer_secret = "r2deGcivhRDxVpalFw0JNc4rtYTwemLjhGhRR5TiYXOAnpS8eA"
+access_token = ""
+access_token_secret = ""
+consumer_key = ""
+consumer_secret = ""
+
+hundredMB = 1024 * 1000 * 100
 
 # modified version of the basic StreamListener from Tweepy
 class JSONStream(tweepy.StreamListener):
-    def __init__(self, timeLimit, outputFileName):
+    def __init__(self, timeLimit):
         self.timeStart = time.time()
         self.timeLimit = 60 * timeLimit
-        self.outputFile = open(outputFileName, 'a')
         super(JSONStream, self).__init__()
 
     def on_status(self, status):
+        global f, fileCount
         if (time.time() - self.timeStart) < self.timeLimit:
-            self.outputFile.write(json.dumps(status._json) + '\n')
+            if f.tell() > hundredMB:
+                f.close()
+                fileCount += 1
+                outputPath = "data/tweets_" + str(fileCount) + ".json"
+                f = open(outputPath, 'a')
+            f.write(json.dumps(status._json) + '\n')
         else:
-            self.outputFile.close()
+            f.close()
             return False
 
-if __name__ == "__main__":
-    # parse command line arguments
-    if (len(sys.argv) != 3):
-        sys.stderr.write("[Error]: Too few arguments\n")
-        sys.stderr.write("Usage: app.py <Time Limit (minutes)> <Output File>\n")
-        sys.exit(1)
+    def on_error(self, status_code):
+        sys.stderr.write("[Error] - Unable to process tweets, " + str(status_code) + '\n')
+        f.close()
+        return False
 
-    TIME_LIMIT = int(sys.argv[1])
-    FILENAME = str(sys.argv[2])
+# create directory for data files
+try:
+    os.makedirs("data")
+except OSError as exception:
+    if exception.errno != errno.EEXIST:
+        raise
 
-    # authentication
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth)
+# setup output files
+fileCount = 0
+outputPath = "data/tweets_" + str(fileCount) + ".json"
+f = open(outputPath, 'a')
 
-    # gather current trends in the US
-    #rawTrends = api.trends_place(id=2442047)
-    #trends = list()
-    #sys.stderr.write("Currently trending topics:\n")
-    #for trend in rawTrends[0]["trends"]:
-    #    trends.append(trend["name"])
-    #    sys.stderr.write("\t> " + trend["name"] + "\n")
+# parse command line arguments
+if (len(sys.argv) != 2):
+    sys.stderr.write("[Error]: Too few arguments\n")
+    sys.stderr.write("Usage: app.py <Time Limit (minutes)>\n")
+    sys.exit(1)
 
-    # bounding box
-    box = [-125.0011, 24.9493, -66.9326, 49.5904]
+TIME_LIMIT = int(sys.argv[1])
 
-    # gather tweets until time runs out
-    jsonstream = JSONStream(outputFileName=FILENAME, timeLimit=TIME_LIMIT)
-    stream = tweepy.Stream(auth=api.auth, listener=jsonstream)
-    sys.stderr.write("\nCollecting tweets for " + str(TIME_LIMIT) + " minutes... ")
-    stream.filter(locations=box)
-    sys.stderr.write("Done!\n")
+
+# authentication
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+# gather current trends in the US
+#rawTrends = api.trends_place(id=2442047)
+#trends = list()
+#sys.stderr.write("Currently trending topics:\n")
+#for trend in rawTrends[0]["trends"]:
+#    trends.append(trend["name"])
+#    sys.stderr.write("\t> " + trend["name"] + "\n")
+
+# bounding box
+box = [-125.0011, 24.9493, -66.9326, 49.5904]
+
+# gather tweets until time runs out
+jsonstream = JSONStream(timeLimit=TIME_LIMIT)
+stream = tweepy.Stream(auth=api.auth, listener=jsonstream)
+
+print("Collecting tweets for " + str(TIME_LIMIT) + " minutes...")
+stream.filter(locations=box)
+print("Done!")
